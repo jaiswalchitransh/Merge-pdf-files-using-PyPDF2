a70255f4e49d44d0dfaafb714463d352c39baf25
Chitransh Jaiswal <jaiswalchitransh553@gmail.com>
2024-02-19 12:38:18 +0530

    Update merger.py

--- merger.py
+++ merger.py
@@ -3,9 +3,9 @@ from PyPDF2 import PdfWriter
 import os
 
 merger = PdfWriter()
-files = [file for file in os.listdir() if file.endswith(".pdf")]
+pdfs = [file for file in os.listdir() if file.endswith(".pdf")]
 
-for pdf in files:
+for pdf in pdfs:
     merger.append(pdf)
 
 merger.write("merged.pdf")
