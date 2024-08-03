647e7b548b052ca0ba269ba45d4a9b1902efd867
Chitransh Jaiswal <jaiswalchitransh553@gmail.com>
2024-08-04 00:35:48 +0530

    Add files via upload

---
+++ merger.py
@@ -0,0 +1,13 @@
+# Merge pdf files using PyPDF2
+from PyPDF2 import PdfWriter
+import os
+
+merger = PdfWriter()
+pdfs = [file for file in os.listdir() if file.endswith(".pdf")]
+
+for pdf in pdfs:
+    merger.append(pdf)
+
+merger.write("merged.pdf")
+print("PDF Files have benn merged successfully")
+merger.close()
